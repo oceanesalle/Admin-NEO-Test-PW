7f58ec3d1aa23e73e69c62acdd5c95e531d4b166
oceansalle <oceanesalleiae@gmail.com>
2022-08-08 14:54:44 +0200

    fixing white blank screen

---
+++ admin/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+import { Header } from '../Header/Header';
+
+const Home = () => {
+  return (
+    <div className="left-h">
+      <Header />
+      </div>
+  )
+}
+
+export default Home
--- admin/src/index.js
+++ admin/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-
-ReactDOM.render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+
 );
 
 
